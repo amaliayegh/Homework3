450966d05a72784b8f4a43317f023e5cf59639a8
Amalya Yeghiazarian <amaliayeghiazarian@yahoo.com>
2017-09-11 23:20:05 +0400

    create style.css and homework2.js in assets folder.

---
+++ assets/homework2.js
@@ -0,0 +1,21 @@
+let num1 = 5;
+let num2 = 12;
+
+
+let result;
+let name = "Amalya";
+let CurrentYear = 2017, DateOfBith = 1995, Age;
+
+result = num1 + num2;
+console.log('result: num1 + num2 =', result);
+
+num1 = (num1 + num2)
+console.log('new value: num1 + num2 =', num2);
+
+num1 = (num1 + num2)/2
+console.log('new value: (num1 + num2)/2 =', num1);
+
+console.log('My name is', name);
+
+Age = CurrentYear - DateOfBith;
+console.log('Age is: CurrentYear - DateOfBith', Age);
